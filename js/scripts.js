d5c2748d81fc215763465fabaac30ca07789834a
Kyle Mellander <kylemellander@gmail.com>
2015-09-18 08:36:16 -0700

    Add addTopping prototype to Pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 function Pizza(size) {
   this.size = size;
+  this.toppings = [];
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
 }
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,10 @@ describe("Pizza", function() {
     var testPizza = new Pizza("large");
     expect(testPizza.size).to.equal("large");
   })
+
+  it("adds a topping to a pizza", function() {
+    var testPizza = new Pizza("large");
+    testPizza.addTopping("Pepperoni");
+    expect(testPizza.toppings).to.eql(["Pepperoni"]);
+  })
 })
